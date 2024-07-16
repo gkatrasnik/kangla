3a376a38597d7959b075218fe3314a78b700a02e
gkatrasnikmxp <gkatrasnik@marinexchange.com>
2024-07-16 15:11:53 +0200

    structure changed
    structure changed

--- kangla-backend.Infrastructure/obj/Release/net8.0/Infrastructure.AssemblyInfo.cs
+++ kangla-backend.Infrastructure/obj/Release/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+724168a3a9a6779a22485f03adfc2d655cc593a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
