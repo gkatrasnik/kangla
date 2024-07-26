733d94832022de9f26dfad941611727f1278e848
gkatrasnikmxp <gkatrasnik@marinexchange.com>
2024-07-26 13:17:23 +0200

    created service and repository for watering events

---
+++ src/kangla-backend.Application/Services/WateringEventService.cs
@@ -0,0 +1,33 @@
+﻿using Application.DTO;
+using Application.Interfaces;
+using AutoMapper;
+using Domain.Model;
+
+
+namespace Application.Services
+{
+    public class WateringEventService : IWateringEventService
+    {
+        private readonly IWateringEventRepository _repository;
+        private readonly IMapper _mapper;
+
+        public WateringEventService(IWateringEventRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<WateringEventResponseDto>> GetWateringEventsForDeviceAsync(int deviceId)
+        {
+            var wateringEvents = await _repository.GetWateringEventsByDeviceIdAsync(deviceId);
+            return _mapper.Map<IEnumerable<WateringEventResponseDto>>(wateringEvents);
+        }
+
+        public async Task<WateringEventResponseDto> CreateWateringEventAsync(WateringEventCreateRequestDto wateringEvent)
+        {
+            var entity = _mapper.Map<WateringEvent>(wateringEvent);
+            await _repository.AddWateringEventAsync(entity);
+            return _mapper.Map<WateringEventResponseDto>(entity);
+        }
+    }
+}
---
+++ src/kangla-backend.Infrastructure/Repositories/WateringEventRepository.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Model;
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public class WateringEventRepository : IWateringEventRepository
+{
+    private readonly WateringContext _context;
+
+    public WateringEventRepository(WateringContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<WateringEvent>> GetWateringEventsByDeviceIdAsync(int deviceId)
+    {
+        return await _context.WateringEvents
+                             .Where(e => e.WateringDeviceId == deviceId)
+                             .ToListAsync();
+    }
+
+    public async Task AddWateringEventAsync(WateringEvent wateringEvent)
+    {
+        _context.WateringEvents.Add(wateringEvent);
+        await _context.SaveChangesAsync();
+    }
+}
