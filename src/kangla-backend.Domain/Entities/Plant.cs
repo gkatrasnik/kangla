ac67cff8cc9e2983eb97b17cf08803b253505aa2
Gašper Katrašnik <gkatrasnik@gmail.com>
2024-11-01 18:28:04 +0100

    cooments added

--- src/kangla-backend.Domain/Entities/Plant.cs
+++ src/kangla-backend.Domain/Entities/Plant.cs
@@ -5,7 +5,10 @@ namespace Domain.Entities
 {
     public class Plant : IEntity
     {
-        [Required]
+        /// <summary>
+        /// User id from Microsoft.AspNetCore.Identity that is owner of the plant.
+        /// </summary>
+        [Required]        
         public string UserId { get; set; } = default!;
         [Required]
         [StringLength(50, ErrorMessage = "Name must be less than 50 characters long.")]
@@ -20,13 +23,13 @@ namespace Domain.Entities
         public string? Notes { get; set; }
         [Required]
         [Range(1, 365, ErrorMessage = "Watering interval must be between 1 and 365 days.")]
+        /// <summary>
+        /// WateringInterval unit is days.
+        /// </summary>
         public int WateringInterval { get; set; } = 0;
         [StringLength(500, ErrorMessage = "Watering instructions must be less than 500 characters long.")]
         public string? WateringInstructions { get; set; } = default!;
-        public List<WateringEvent>? WateringEvents { get; set; }       
-        /// <summary>
-        /// User id from Microsoft.AspNetCore.Identity that is owner of the plant.
-        /// </summary>
+        public List<WateringEvent>? WateringEvents { get; set; }        
         public WateringDevice? WateringDevice { get; set; }        
         public int? ImageId { get; set; } = default!;
     }
