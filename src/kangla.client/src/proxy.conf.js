6f86c2b71e2d954b5c40662b1a6c118d3f22acca
gkatrasnikmxp <gkatrasnik@gmail.com>
2024-12-30 14:19:24 +0100

    all api calls should be moved to /api
    all api calls should be moved to /api

--- src/kangla.client/src/proxy.conf.js
+++ src/kangla.client/src/proxy.conf.js
@@ -1,12 +1,12 @@
 const { env } = require('process');
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7075';
+  env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7075';
 
 const PROXY_CONFIG = [
   {
     context: [
-      "/api/**",
+      "/api",
     ],
     target,
     secure: false
