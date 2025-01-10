34a32075b59484daab667e2d4ce0a5cc0839dea3
gkatrasnikmxp <gkatrasnik@gmail.com>
2025-01-10 14:42:35 +0100

    precautions to only allow uploading images

---
+++ src/kangla.Domain/Constants/ImageConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace kangla.Domain.Constants
+{
+    public static class ImageConstants
+    {
+        public static readonly string[] AllowedContentTypes =
+        {
+            "image/jpeg",
+            "image/png",
+            "image/gif",
+            "image/webp"
+        };
+    }
+}
